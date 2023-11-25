fae0a8ccc84c3cfe51267fcc73bb0e10d9c0e90a
Serhii Podhornyi <serhii.podhornyi@gmail.com>
2023-11-25 19:31:31 +0200

    add new chalenge

---
+++ 8kyu/quadrants.js
@@ -0,0 +1,42 @@
+// Task
+// Given a point in a Euclidean plane (x and y), return the quadrant the point exists in: 1, 2, 3 or 4 (integer). x and y are non-zero integers, therefore the given point never lies on the axes.
+
+// Examples
+// (1, 2)     => 1
+// (3, 5)     => 1
+// (-10, 100) => 2
+// (-1, -9)   => 3
+// (19, -56)  => 4
+// Reference
+// QuadrantsQuadrants
+// There are four quadrants:
+
+// First quadrant, the quadrant in the top-right, contains all points with positive X and Y
+// Second quadrant, the quadrant in the top-left, contains all points with negative X, but positive Y
+// Third quadrant, the quadrant in the bottom-left, contains all points with negative X and Y
+// Fourth quadrant, the quadrant in the bottom-right, contains all points with positive X, but negative Y
+
+// My solution:
+
+// function quadrant(x, y) {
+//   let a = "";
+//   if (x > 0 && y > 0) a = 1;
+
+//   if (x < 0 && y > 0) a = 2;
+//   if (x < 0 && y < 0) a = 3;
+
+//   if (x > 0 && y < 0) a = 4;
+
+//   console.log("a: ", a);
+//   return;
+// }
+
+function quadrant(x, y) {
+  let a = "";
+  if (x > 0) y > 0 ? (a = 1) : (a = 4);
+  if (x < 0) y > 0 ? (a = 2) : (a = 3);
+  console.log("a: ", a);
+  return;
+}
+
+quadrant(1, 2);
